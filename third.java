36c668188d9051d1a80d4e2056362409cf0cb6c4
Arup Barik <arupbarik323@gmail.com>
2026-04-14 22:55:00 +0530

    add two numbers

--- third.java
+++ third.java
@@ -4,5 +4,9 @@ public class third {
     public static void main(String[] args) {
         System.out.println("third part");
         //Narrowing Casting (manually) - converting a larger type to a smaller size type
+        int a=28;
+        int b=56;
+        int total= a+b;
+        System.out.println("Total: "+total);
     }
 }
