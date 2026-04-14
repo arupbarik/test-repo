a529553e335d9f5879e65a9d3f9bce238e62e9ef
Arup Barik <arupbarik323@gmail.com>
2026-04-14 22:48:34 +0530

    create new branch

---
+++ third.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class third {
+    public static void main(String[] args) {
+        System.out.println("third part");
+        //Narrowing Casting (manually) - converting a larger type to a smaller size type
+    }
+}
